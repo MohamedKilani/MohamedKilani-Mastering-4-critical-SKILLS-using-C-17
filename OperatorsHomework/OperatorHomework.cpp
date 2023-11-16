8d03c7bcf2840301e20a3e4832a6bcd09a4aec29
MoKilani <46265887+MohamedKilani@users.noreply.github.com>
2023-11-16 19:25:42 +0200

    added operators Homework

---
+++ OperatorsHomework/OperatorHomework.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+int main(){
+  
+	// operators Homework #1
+	/*
+	
+	int a = 0, b{ 1 };
+	cout << a++ << "\n"; // 0
+	cout << ++a << "\n"; // 2
+	a += 2 * b + 1;  //a = 5
+	b = ++a * 2; //a = 6 , b = 12
+	cout << a << " " << b << endl;
+	b = a; // b = 6
+	a = 12 + a / 3 / 2 - 2 * 2;// a = 9
+	cout << a << endl;
+	a = b; // a = 6
+	a = ((12 + a) / 3 / 2 - 2) * 2; //a = 2
+	cout << a << endl;
+
+	//	operators Homework #2
+	int a = 1, b = 1, c;
+	cout << (c = a + b, a = b, b = c,
+			c = a + b, a = b, b = c,
+			c = a + b, a = b, b = c,
+			c = a + b, a = b, b = c) << endl;
+	// c =8
+
+	//Operators Homework #3
+	int a = 210;
+	a /= 2;
+	cout << a << endl;// 105
+	cout << (a /= 3) << endl; //35
+	cout << (a /= 5) << endl;//7
+	cout << (a /= 7) << endl;// 1
+	cout << (2 + 3) * (5 - (-3)) / 5 / 8 << endl; //1
+	a = 10;
+	cout << a++ + 10 << endl; //20
+	cout << ++a + 10 << endl;//22
+	cout << a-- + 10 << endl;//22
+	cout << --a + 10 << endl;//20
+	int b = 20;
+	cout << a++ + ++b << endl;//31
+	cout << a << endl;//11
+	++a += 10;//22
+	cout << a << endl;
+	
+	
+	
+	//Logical Operators Homework #1
+	int NoB, NoG, NoT;
+	cout << "Enter number of Boys" << endl;
+	cin >> NoB;
+	cout << "Enter number of Girls" << endl;
+	cin >> NoG;
+	cout << "Enter number of Teachers" << endl;
+	cin >> NoT;
+	bool nbGreater25 = NoB > 25;
+	bool NgLessOrEq30 = NoG <= 30;
+	cout << nbGreater25 << " " << NgLessOrEq30 << endl;
+	cout << (NoB > 20 && NoT > 2 || NoG > 30 && NoT > 4)<<endl;
+	cout << (NoB < 60 || NoG < 70)<<endl;
+	cout << !(NoB >= 60 || NoG >= 70)<<endl;
+	cout << (NoB == (NoG + 10))<<endl;
+	cout << ((NoB - NoG) > (10) || (NoT > 5)) << endl;
+	cout << ((NoB == 10 + NoG) || (NoG == 15 + NoB)) << endl;
+	
+	
+
+	// Divison & Modulus Homework #1
+	double n1, n2, n3, n4, n5;
+	cout << " enter 5 numbers" << endl;
+	cin >> n1 >> n2 >> n3 >> n4 >> n5;
+	double A = (n1 + n2 + n3 + n4 + n5) / 5.0;
+	double B = (n1 + n2 + n3) / (n4 + n5);
+	double C = ((n1 + n2 + n3) / 3) / ((n4 + n5)/ 2);
+	cout << A << endl << B << endl << C;
+
+	// Divison & Modulus Homework #2
+	double a, b;
+	cin >> a >> b;
+	//int  adividb = double(a / b);
+	double f = a / b;
+	cout << f - int(f) << endl;
+	
+	*/
+		
+
+
+  return 0;
+}
